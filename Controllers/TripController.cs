cdc2185dcac11fa056aacdb83a066bdd5209fc3b
mahesh <lanka.maheshvaas@gmail.com>
2020-09-17 01:04:06 +0530

    Trip Details

---
+++ Controllers/TripController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using MyTrips.Models;
+namespace MyTrips.Controllers
+{
+    public class TripController : Controller
+    {
+        // GET: Trip
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            using(DBModel db = new DBModel())
+            {
+                List<Trip> tripList = db.Trips.ToList<Trip>();
+                return Json(new {data = tripList}, JsonRequestBehavior.AllowGet); 
+
+            }
+        }
+        [HttpGet]
+        public ActionResult AddorEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new Trip());
+            }
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.Trips.Where(x => x.Id==id).FirstOrDefault<Trip>());
+                }
+            }
+        }
+        
+        [HttpPost]
+        public ActionResult AddorEdit(Trip trp)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if(trp.Id ==0)
+                {
+                    db.Trips.Add(trp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+
+                }
+                else
+                {
+                    db.Entry(trp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+
+                }
+            }
+                
+        }
+        [HttpPost]
+        public ActionResult Delete(int id )
+        {
+            using (DBModel db = new DBModel())
+            {
+                Trip trp = db.Trips.Where(x => x.Id == id).FirstOrDefault<Trip>();
+                db.Trips.Remove(trp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+
+            }
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyTrips.MvcApplication" Language="C#" %>
